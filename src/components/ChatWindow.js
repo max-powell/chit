1f01d3d3a3d3d29fd1b0224a365d0c002f36cdbd
max-powell <github email address>
2019-07-10 10:23:59 +0100

    chat window automatically scrolls to bottom when new message comes in

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Message from './Message'
 
-const ChatWindow = ({messages, userId}) => (
-  <div id='chat-window'>
-    {
-      messages.map(m => <Message key={m.id} message={m} userId={userId} />)
-    }
-  </div>
-)
+class ChatWindow extends Component {
+
+  windowRef = React.createRef()
+
+  componentDidUpdate () {
+    const { current: window } = this.windowRef
+    debugger;
+    window.scrollTo(0, window.scrollHeight)
+  }
+
+  render() {
+    const { messages, userId } = this.props
+    const { windowRef } = this
+
+    return (
+      <div id='chat-window' ref={windowRef}>
+        {
+          messages.map(m => <Message key={m.id} message={m} userId={userId} />)
+        }
+      </div>
+    )
+  }
+
+}
 
 export default ChatWindow
