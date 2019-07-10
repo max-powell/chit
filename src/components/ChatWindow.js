2ccecedb0961136f71755902b87bb420269447d5
max-powell <github email address>
2019-07-10 11:02:40 +0100

    renamed chatWindow ref

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -4,20 +4,28 @@ import Message from './Message'
 
 class ChatWindow extends Component {
 
-  windowRef = React.createRef()
+  chatWindowRef = React.createRef()
 
-  componentDidUpdate () {
-    const { current: window } = this.windowRef
-    debugger;
-    window.scrollTo(0, window.scrollHeight)
+  scrollToBottom = () => {
+    const { current: chatWindow } = this.chatWindowRef
+    chatWindow.scrollTo(0, chatWindow.scrollHeight)
+  }
+
+  componentDidMount () {
+    const { current: chatWindow } = this.chatWindowRef
+    this.scrollToBottom()
+  }
+
+  componentDidUpdate (prevProps, prevState, snapshot) {
+    this.scrollToBottom()
   }
 
   render() {
     const { messages, userId } = this.props
-    const { windowRef } = this
+    const { chatWindowRef } = this
 
     return (
-      <div id='chat-window' ref={windowRef}>
+      <div id='chat-window' ref={chatWindowRef}>
         {
           messages.map(m => <Message key={m.id} message={m} userId={userId} />)
         }
