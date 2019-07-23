e95d3f988850160bb8b9778ca857e3ece041b8ed
max-powell <github email address>
2019-07-23 11:23:39 +0100

    split chat window into container and display components

--- src/components/ChatDisplay.js
+++ src/components/ChatDisplay.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-import ChatWindow from './ChatWindow'
+import ChatWindowContainer from '../containers/ChatWindowContainer'
 import NewMessageContainer from '../containers/NewMessageContainer'
 
 const ChatDisplay = ({chat, userId, selectChat, handleReceivedMessage}) => (
   <div id='chat-display'>
     <button onClick={() => selectChat(null)}>Back to chat list</button>
-    <ChatWindow
+    <ChatWindowContainer
       messages={chat.messages}
       chat={chat}
       userId={userId}
